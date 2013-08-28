611145505aefebbdaabaf5ca06059bdde0b11c88
Pauly Myjavec <Pauly.Myjavec@team.telstra.com>
2013-08-28 18:40:09 +1000

    My bundle is awesome

--- lib/monkey/hiera.rb
+++ lib/monkey/hiera.rb
@@ -1,10 +1,10 @@
 require 'validator/config'
 
 # MonkeyPatch of HieraPuppet for compilation to override the hiera lookup function and always return a value
-if Validator.config.hiera_validation == false
-  module HieraPuppet
-    def self.lookup key, default, scope, override, resolution_type
-      return "foo"
-    end
+# As usual with monkey patches, please use this sparingly. It should only be included in instances where the
+# user has not requested hiera validation.
+module HieraPuppet
+  def self.lookup key, default, scope, override, resolution_type
+    return "test"
   end
 end
